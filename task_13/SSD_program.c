234e942ca935901c41c4f670360b26d9046b83c3
Ahmed Mahmoud <ahmedmahmo431@gmail.com>
2025-08-02 01:47:19 +0300

    task_13

--- task_13/SSD_program.c
+++ task_13/SSD_program.c
@@ -1,7 +1,6 @@
 /*
 *<<<<<<<<<<SSD_program.c>>>>>>>>>>
 *
-* Enhanced version with multiplexing support
 * Author : Ahmed Mahmoud
 * Layer : HAL
 * SWC : SSD
@@ -21,23 +20,11 @@
 
 static u_8 Local_u8SSDNumbers[10] = SSD_NUMBER_ARR;
 
-//====================================================================================================================================================
-/*
-* Brief : This Function initialize the port which connected to 7 Seg leds as output pins ( 8 Pins or Port )
-* Parameters : Copy_structConfig --> SSD configuration struct
-* return : void
-*/
 void SSD_voidInitialDataPort(SSD_Type Copy_structConfig)
 {
     DIO_enumSetPortDirection(Copy_structConfig.DataPort, 0xFF);
 }
 
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-/*
-* Brief : This Function write Number on 7 seg [ 0 : 9 ]
-* Parameters : Copy_structConfig --> SSD configuration, Copy_u8Number --> digit to display
-* return : void
-*/
 void SSD_voidSendNumber(SSD_Type Copy_structConfig, u_8 Copy_u8Number)
 {
     if(Copy_structConfig.Type == SSD_COMMON_CATHODE) {
@@ -48,26 +35,16 @@ void SSD_voidSendNumber(SSD_Type Copy_structConfig, u_8 Copy_u8Number)
     }
 }
 
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-/*
-* Brief : Display a two-digit number using multiplexing
-* Parameters : Copy_structConfigUnits --> Units SSD config
-*             Copy_structConfigTens --> Tens SSD config
-*             Copy_u8Number --> Number to display (0-99)
-*             Copy_u16DisplayTime --> Total display time in ms
-* return : void
-*/
 void SSD_voidDisplayTwoDigits(SSD_Type Copy_structConfigUnits, SSD_Type Copy_structConfigTens,
                              u_8 Copy_u8Number, u_16 Copy_u16DisplayTime)
 {
-    u_8 Local_u8Units = Copy_u8Number % 10;        // Extract units digit
-    u_8 Local_u8Tens = Copy_u8Number / 10;         // Extract tens digit
-    u_16 Local_u16Cycles = (Copy_u16DisplayTime * 2) / 5; // Reduce total cycles to speed up
+    u_8 Local_u8Units = Copy_u8Number % 10;
+    u_8 Local_u8Tens = Copy_u8Number / 10;
+    u_16 Local_u16Cycles = (Copy_u16DisplayTime * 2) / 5;
 
     for(u_16 i = 0; i < Local_u16Cycles; i++)
     {
-        // Display tens digit
-        if(Local_u8Tens > 0) // Don't show leading zero
+        if(Local_u8Tens > 0)
         {
             SSD_voidEnable(Copy_structConfigTens);
             SSD_voidDisable(Copy_structConfigUnits);
@@ -76,12 +53,10 @@ void SSD_voidDisplayTwoDigits(SSD_Type Copy_structConfigUnits, SSD_Type Copy_str
         }
         else
         {
-            // If no tens digit, still need to wait the same time for balance
             SSD_voidDisable(Copy_structConfigTens);
             _delay_ms(1);
         }
 
-        // Display units digit
         SSD_voidEnable(Copy_structConfigUnits);
         SSD_voidDisable(Copy_structConfigTens);
         SSD_voidSendNumber(Copy_structConfigUnits, Local_u8Units);
@@ -89,14 +64,6 @@ void SSD_voidDisplayTwoDigits(SSD_Type Copy_structConfigUnits, SSD_Type Copy_str
     }
 }
 
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-/*
-* Brief : Enhanced countdown function with proper two-digit display
-* Parameters : Copy_u8StartNumber --> Starting countdown number
-*             Copy_structConfigUnits --> Units SSD config
-*             Copy_structConfigTens --> Tens SSD config
-* return : void
-*/
 void SSD_voidCountdown(u_8 Copy_u8StartNumber, SSD_Type Copy_structConfigUnits, SSD_Type Copy_structConfigTens)
 {
     for(u_8 i = Copy_u8StartNumber; i > 0; i--)
@@ -105,12 +72,6 @@ void SSD_voidCountdown(u_8 Copy_u8StartNumber, SSD_Type Copy_structConfigUnits, 
     }
 }
 
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-/*
-* Brief : This Function enable common pin
-* Parameters : Copy_structConfig --> SSD configuration
-* return : void
-*/
 void SSD_voidEnable(SSD_Type Copy_structConfig)
 {
     if(Copy_structConfig.Type == SSD_COMMON_CATHODE) {
@@ -123,12 +84,6 @@ void SSD_voidEnable(SSD_Type Copy_structConfig)
     }
 }
 
-//----------------------------------------------------------------------------------------------------------------------------------------------------
-/*
-* Brief : This Function disable common pin
-* Parameters : Copy_structConfig --> SSD configuration
-* return : void
-*/
 void SSD_voidDisable(SSD_Type Copy_structConfig)
 {
     if(Copy_structConfig.Type == SSD_COMMON_CATHODE) {
--- task_13/main.c
+++ task_13/main.c
@@ -1,7 +1,6 @@
 /*
 *<<<<<<<<<<main.c>>>>>>>>>>
 *
-* Traffic Light with Multiplexed SSD Countdown
 * Author : Ahmed Mahmoud
 * Layer : APP
 *
@@ -18,7 +17,6 @@
 
 int main(void)
 {
-    // Initialize LEDs
     LED_Type ledRed = {LED_PORTA, LED_PIN0, ACTIVE_HIGH};
     LED_Type ledYellow = {LED_PORTA, LED_PIN1, ACTIVE_HIGH};
     LED_Type ledGreen = {LED_PORTA, LED_PIN2, ACTIVE_HIGH};
@@ -27,37 +25,31 @@ int main(void)
     LED_voidInit(ledYellow);
     LED_voidInit(ledGreen);
 
-    // Initialize Two SSDs for multiplexing
-    SSD_Type ssdUnits = {SSD_COMMON_ANODE, SSD_PORTC, SSD_PORTD, SSD_PIN0}; // Units digit
-    SSD_Type ssdTens  = {SSD_COMMON_ANODE, SSD_PORTC, SSD_PORTD, SSD_PIN1}; // Tens digit
+    SSD_Type ssdUnits = {SSD_COMMON_ANODE, SSD_PORTC, SSD_PORTD, SSD_PIN0};
+    SSD_Type ssdTens  = {SSD_COMMON_ANODE, SSD_PORTC, SSD_PORTD, SSD_PIN1};
 
-    // Initialize both SSDs
-    SSD_voidInitialDataPort(ssdUnits);  // Only need to init data port once (shared)
+    SSD_voidInitialDataPort(ssdUnits);
 
     while(1)
     {
-        // RED on for 15 seconds (countdown 15->1)
         LED_voidOn(ledRed);
         LED_voidOff(ledYellow);
         LED_voidOff(ledGreen);
 
         SSD_voidCountdown(15, ssdUnits, ssdTens);
 
-        // YELLOW on for 5 seconds (countdown 5->1)
         LED_voidOn(ledYellow);
         LED_voidOff(ledRed);
         LED_voidOff(ledGreen);
 
         SSD_voidCountdown(5, ssdUnits, ssdTens);
 
-        // GREEN on for 10 seconds (countdown 10->1)
         LED_voidOn(ledGreen);
         LED_voidOff(ledRed);
         LED_voidOff(ledYellow);
 
         SSD_voidCountdown(10, ssdUnits, ssdTens);
 
-        // YELLOW again for 5 seconds (countdown 5->1)
         LED_voidOn(ledYellow);
         LED_voidOff(ledRed);
         LED_voidOff(ledGreen);
